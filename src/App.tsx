0cb4ae6c6b8cb365a962d85886afc97f8d6430bb
odedmo <odedmoran222@gmail.com>
2022-11-19 19:35:41 +0200

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,29 @@
-import styled from "@emotion/styled";
-import { Box, CssBaseline, Paper, Typography } from "@mui/material";
+import { Box, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./theme";
-
-const StyledTypography = styled(Typography)(({ theme: Theme }) => ({
-  color: theme.palette.primary.dark
-}));
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { routes as appRoutes } from "./routes";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box
-        height="100vh"
-        display="flex"
-        justifyContent="center"
-        alignItems="center"
-        flexDirection="column"
-      >
-        <Paper
-          elevation={3}
-          sx={{ padding: "1rem", backgroundColor: "secondary.light" }}
-        >
-          <StyledTypography variant="h1">Starter App</StyledTypography>
-        </Paper>
+      <Box height="100vh" display="flex" flexDirection="column">
+        <Router>
+          <Navbar />
+          <Routes>
+            {appRoutes.map((route) => (
+              <Route
+                key={route.key}
+                path={route.path}
+                element={<route.component />}
+              />
+            ))}
+          </Routes>
+          <Footer />
+        </Router>
       </Box>
     </ThemeProvider>
   );
